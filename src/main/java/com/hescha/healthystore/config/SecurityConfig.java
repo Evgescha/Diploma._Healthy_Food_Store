9c97aba175a27603dbb9b5cfdef1fc9251c7bc21
Yauhescha <evgescha.maslov007@yandex.ru>
2023-04-05 00:21:46 +0300

    category page

--- src/main/java/com/hescha/healthystore/config/SecurityConfig.java
+++ src/main/java/com/hescha/healthystore/config/SecurityConfig.java
@@ -2,28 +2,45 @@ package com.hescha.healthystore.config;
 
 import com.hescha.healthystore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Autowired
     private UserService userDetailsService;
+    @Autowired
+    private ApplicationContext context;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
         auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("{noop}user") // {noop} указывает, что пароль не будет кодироваться
-                .roles("USER");
+                .withUser("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN");
     }
+
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        DefaultWebSecurityExpressionHandler handler =
+                new DefaultWebSecurityExpressionHandler();
+        handler.setApplicationContext(context);
+        web.expressionHandler(handler);
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/hescha/healthystore/controller/CategoryController.java
+++ src/main/java/com/hescha/healthystore/controller/CategoryController.java
@@ -6,7 +6,6 @@ import com.hescha.healthystore.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ public class CategoryController {
     public static final String CURRENT_ADDRESS = "/" + API_PATH;
     public static final String MESSAGE = "message";
     public static final String THYMELEAF_TEMPLATE_ALL_ITEMS_PAGE = API_PATH;
-    public static final String THYMELEAF_TEMPLATE_ONE_ITEM_PAGE = THYMELEAF_TEMPLATE_ALL_ITEMS_PAGE + "-one";
     public static final String THYMELEAF_TEMPLATE_EDIT_PAGE = THYMELEAF_TEMPLATE_ALL_ITEMS_PAGE + "-edit";
     public static final String REDIRECT_TO_ALL_ITEMS = "redirect:" + CURRENT_ADDRESS;
 
@@ -37,12 +35,6 @@ public class CategoryController {
         return THYMELEAF_TEMPLATE_ALL_ITEMS_PAGE;
     }
 
-    @GetMapping("/{id}")
-    public String read(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("entity", service.read(id));
-        return THYMELEAF_TEMPLATE_ONE_ITEM_PAGE;
-    }
-
     @GetMapping(path = {"/edit", "/edit/{id}"})
     public String editPage(Model model, @PathVariable(name = "id", required = false) Long id) {
         if (id == null) {
@@ -50,9 +42,6 @@ public class CategoryController {
         } else {
             model.addAttribute("entity", service.read(id));
         }
-
-        model.addAttribute("product_list", productService.readAll());
-
         return THYMELEAF_TEMPLATE_EDIT_PAGE;
     }
 
@@ -67,7 +56,6 @@ public class CategoryController {
                 ra.addFlashAttribute(MESSAGE, "Creating failed");
                 e.printStackTrace();
             }
-            return REDIRECT_TO_ALL_ITEMS;
         } else {
             try {
                 service.update(entity.getId(), entity);
@@ -76,8 +64,8 @@ public class CategoryController {
                 e.printStackTrace();
                 ra.addFlashAttribute(MESSAGE, "Editing failed");
             }
-            return REDIRECT_TO_ALL_ITEMS + "/" + entity.getId();
         }
+        return REDIRECT_TO_ALL_ITEMS;
     }
 
     @GetMapping("/{id}/delete")
